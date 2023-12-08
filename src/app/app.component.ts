006b22b6415df2c1189dfd707db0435a9d7f152d
jerry694 <wafoaristide@gmail.com>
2023-12-08 17:56:15 +0100

    init

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'aristidePorteFolio';
+  name = 'Wafo Aristide';
+  classe = 'Inge 4 ISI';
+  role = 'Frontend Développeur';
+
+}
